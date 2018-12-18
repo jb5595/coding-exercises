dbc36dcbbb21172872c2fb9d98d5953c5154dd71
jb5595 <jacobberman1995@gmail.com>
2018-12-18 16:10:23 -0500

    finished working solution for fibonacci while loop and placeholder array to store last 2 fibonacci numbers

--- code/fibonacci.rb
+++ code/fibonacci.rb
@@ -2,5 +2,24 @@ class Fibonacci
   # Implement a method that will calculate the Nth number of the Fibonacci
   # sequence (http://en.wikipedia.org/wiki/Fibonacci_number)
   def self.calculate(n)
+    # Immediately returns 0 if n = 0
+    if n == 0
+      return 0
+    end
+    # initializes placeholder array to keep track of last 2 numbers generated
+    last_two_fib = [0, 1]
+    i = 1
+    # generates each term in Fibonacci sequence until n is reached
+    while (i < n )
+      # Computes next value by adding last two numbers
+      new = last_two_fib[0] + last_two_fib[1]
+      # move previous last number to to first space in placeholder array
+      last_two_fib[0] = last_two_fib[1]
+      # Sets second space in array to equal to newly generated number
+      last_two_fib[1] = new
+      i = i +1
+    end
+    # Returns last number generated
+    return last_two_fib.last
   end
 end
